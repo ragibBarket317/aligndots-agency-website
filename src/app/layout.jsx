4dfc2d3e4901b996392631e04aaf83ca25d87782
Ragib Barket <ragibrahman86317@gmail.com>
2025-02-06 15:57:40 +0600

    add favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,13 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { icons } from 'lucide-react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Aligndots- Marketing Agency',
   description: 'We do marketing from another galaxy',
+  icons: '/aligndots.ico',
 }
 
 export default function RootLayout({ children }) {
